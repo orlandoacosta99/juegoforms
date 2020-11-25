ac117667e627afdc5f0132ddf1df50b75d7a9d78
orlando acosta <oacosta00309@universidadean.edu.co>
2020-11-24 22:51:03 -0500

    se añadieron cambios forms

--- juego/Form1.Designer.cs
+++ juego/Form1.Designer.cs
@@ -32,6 +32,10 @@ namespace juego
             this.Salir = new System.Windows.Forms.Button();
             this.Limpiar = new System.Windows.Forms.Button();
             this.groupCodificador = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtSinDecimal = new System.Windows.Forms.RichTextBox();
+            this.txtDatosCodificados = new System.Windows.Forms.RichTextBox();
             this.btnCodificar = new System.Windows.Forms.Button();
             this.textCodificar = new System.Windows.Forms.TextBox();
             this.comboBoxCodificar = new System.Windows.Forms.ComboBox();
@@ -40,17 +44,22 @@ namespace juego
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.groupDecodificador = new System.Windows.Forms.GroupBox();
+            this.txtDecodificado = new System.Windows.Forms.RichTextBox();
             this.btnDecodificar = new System.Windows.Forms.Button();
             this.textDecodificar = new System.Windows.Forms.RichTextBox();
             this.comboBoxDecodificar = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.txtDatosCodificados = new System.Windows.Forms.RichTextBox();
-            this.txtSinDecimal = new System.Windows.Forms.RichTextBox();
-            this.txtDecodificado = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.Buscar = new System.Windows.Forms.Button();
+            this.comboBoxBuscar = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.groupCodificador.SuspendLayout();
             this.groupDecodificador.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Salir
@@ -79,7 +88,9 @@ namespace juego
             // 
             // groupCodificador
             // 
-            this.groupCodificador.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupCodificador.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupCodificador.Controls.Add(this.richTextBox1);
+            this.groupCodificador.Controls.Add(this.label8);
             this.groupCodificador.Controls.Add(this.txtSinDecimal);
             this.groupCodificador.Controls.Add(this.txtDatosCodificados);
             this.groupCodificador.Controls.Add(this.btnCodificar);
@@ -92,17 +103,54 @@ namespace juego
             this.groupCodificador.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupCodificador.Location = new System.Drawing.Point(42, 60);
             this.groupCodificador.Name = "groupCodificador";
-            this.groupCodificador.Size = new System.Drawing.Size(374, 459);
+            this.groupCodificador.Size = new System.Drawing.Size(374, 547);
             this.groupCodificador.TabIndex = 2;
             this.groupCodificador.TabStop = false;
             this.groupCodificador.Text = "Codificador";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(36, 241);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(303, 65);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(32, 206);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(143, 18);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Palabra Codificada";
+            // 
+            // txtSinDecimal
+            // 
+            this.txtSinDecimal.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSinDecimal.Location = new System.Drawing.Point(33, 464);
+            this.txtSinDecimal.Name = "txtSinDecimal";
+            this.txtSinDecimal.Size = new System.Drawing.Size(303, 65);
+            this.txtSinDecimal.TabIndex = 8;
+            this.txtSinDecimal.Text = "";
+            // 
+            // txtDatosCodificados
+            // 
+            this.txtDatosCodificados.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDatosCodificados.Location = new System.Drawing.Point(34, 348);
+            this.txtDatosCodificados.Name = "txtDatosCodificados";
+            this.txtDatosCodificados.Size = new System.Drawing.Size(303, 65);
+            this.txtDatosCodificados.TabIndex = 7;
+            this.txtDatosCodificados.Text = "";
             // 
             // btnCodificar
             // 
             this.btnCodificar.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.btnCodificar.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCodificar.ForeColor = System.Drawing.Color.DarkRed;
-            this.btnCodificar.Location = new System.Drawing.Point(130, 201);
+            this.btnCodificar.Location = new System.Drawing.Point(122, 167);
             this.btnCodificar.Name = "btnCodificar";
             this.btnCodificar.Size = new System.Drawing.Size(119, 36);
             this.btnCodificar.TabIndex = 6;
@@ -149,7 +197,7 @@ namespace juego
             "x",
             "y",
             "z"});
-            this.comboBoxCodificar.Location = new System.Drawing.Point(34, 159);
+            this.comboBoxCodificar.Location = new System.Drawing.Point(35, 123);
             this.comboBoxCodificar.Name = "comboBoxCodificar";
             this.comboBoxCodificar.Size = new System.Drawing.Size(305, 26);
             this.comboBoxCodificar.TabIndex = 4;
@@ -158,7 +206,7 @@ namespace juego
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(30, 349);
+            this.label4.Location = new System.Drawing.Point(31, 428);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(230, 18);
             this.label4.TabIndex = 3;
@@ -168,7 +216,7 @@ namespace juego
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(30, 246);
+            this.label3.Location = new System.Drawing.Point(31, 317);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(142, 18);
             this.label3.TabIndex = 2;
@@ -178,7 +226,7 @@ namespace juego
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(30, 123);
+            this.label2.Location = new System.Drawing.Point(32, 102);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(86, 18);
             this.label2.TabIndex = 1;
@@ -196,7 +244,7 @@ namespace juego
             // 
             // groupDecodificador
             // 
-            this.groupDecodificador.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupDecodificador.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.groupDecodificador.Controls.Add(this.txtDecodificado);
             this.groupDecodificador.Controls.Add(this.btnDecodificar);
             this.groupDecodificador.Controls.Add(this.textDecodificar);
@@ -211,6 +259,15 @@ namespace juego
             this.groupDecodificador.TabIndex = 0;
             this.groupDecodificador.TabStop = false;
             this.groupDecodificador.Text = "Decodificador";
+            // 
+            // txtDecodificado
+            // 
+            this.txtDecodificado.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDecodificado.Location = new System.Drawing.Point(36, 302);
+            this.txtDecodificado.Name = "txtDecodificado";
+            this.txtDecodificado.Size = new System.Drawing.Size(303, 65);
+            this.txtDecodificado.TabIndex = 9;
+            this.txtDecodificado.Text = "";
             // 
             // btnDecodificar
             // 
@@ -300,38 +357,82 @@ namespace juego
             this.label5.TabIndex = 0;
             this.label5.Text = "Códico Cifrado";
             // 
-            // txtDatosCodificados
+            // groupBox1
             // 
-            this.txtDatosCodificados.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDatosCodificados.Location = new System.Drawing.Point(35, 275);
-            this.txtDatosCodificados.Name = "txtDatosCodificados";
-            this.txtDatosCodificados.Size = new System.Drawing.Size(303, 65);
-            this.txtDatosCodificados.TabIndex = 7;
-            this.txtDatosCodificados.Text = "";
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.Controls.Add(this.richTextBox2);
+            this.groupBox1.Controls.Add(this.Buscar);
+            this.groupBox1.Controls.Add(this.comboBoxBuscar);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(849, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 286);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Palabra Aleatorea";
             // 
-            // txtSinDecimal
+            // richTextBox2
             // 
-            this.txtSinDecimal.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSinDecimal.Location = new System.Drawing.Point(33, 379);
-            this.txtSinDecimal.Name = "txtSinDecimal";
-            this.txtSinDecimal.Size = new System.Drawing.Size(303, 65);
-            this.txtSinDecimal.TabIndex = 8;
-            this.txtSinDecimal.Text = "";
+            this.richTextBox2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(36, 177);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(303, 65);
+            this.richTextBox2.TabIndex = 9;
+            this.richTextBox2.Text = "";
             // 
-            // txtDecodificado
+            // Buscar
             // 
-            this.txtDecodificado.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDecodificado.Location = new System.Drawing.Point(36, 302);
-            this.txtDecodificado.Name = "txtDecodificado";
-            this.txtDecodificado.Size = new System.Drawing.Size(303, 65);
-            this.txtDecodificado.TabIndex = 9;
-            this.txtDecodificado.Text = "";
+            this.Buscar.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.Buscar.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Buscar.ForeColor = System.Drawing.Color.DarkRed;
+            this.Buscar.Location = new System.Drawing.Point(124, 102);
+            this.Buscar.Name = "Buscar";
+            this.Buscar.Size = new System.Drawing.Size(121, 36);
+            this.Buscar.TabIndex = 7;
+            this.Buscar.Text = "Buscar";
+            this.Buscar.UseVisualStyleBackColor = false;
+            // 
+            // comboBoxBuscar
+            // 
+            this.comboBoxBuscar.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxBuscar.FormattingEnabled = true;
+            this.comboBoxBuscar.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.comboBoxBuscar.Location = new System.Drawing.Point(38, 67);
+            this.comboBoxBuscar.Name = "comboBoxBuscar";
+            this.comboBoxBuscar.Size = new System.Drawing.Size(301, 26);
+            this.comboBoxBuscar.TabIndex = 3;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(36, 149);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 18);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Datos";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(33, 40);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 18);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Ronda";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(861, 534);
+            this.ClientSize = new System.Drawing.Size(1255, 619);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupDecodificador);
             this.Controls.Add(this.groupCodificador);
             this.Controls.Add(this.Limpiar);
@@ -342,6 +443,8 @@ namespace juego
             this.groupCodificador.PerformLayout();
             this.groupDecodificador.ResumeLayout(false);
             this.groupDecodificador.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -368,6 +471,14 @@ namespace juego
         private System.Windows.Forms.RichTextBox txtSinDecimal;
         private System.Windows.Forms.RichTextBox txtDatosCodificados;
         private System.Windows.Forms.RichTextBox txtDecodificado;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button Buscar;
+        private System.Windows.Forms.ComboBox comboBoxBuscar;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
 
